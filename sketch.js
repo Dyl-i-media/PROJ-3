a831fe5f0757acea13fd277bf885a5dc7c4e05c4
Dyl-i-media <dylan2.chappell@live.uwe.ac.uk>
2025-12-19 15:44:21 +0000

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+let buildingWidth = 30;
+
+function setup() {
+  createCanvas(600, 400);
+  colorMode(HSB, 360, 100, 100);
+  background(210, 10, 95); 
+  noLoop(); 
+}
+
+function draw() {
+  noStroke();
+
+  // Loop across the canvas to place buildings
+  for (let x = 0; x < width; x += buildingWidth) {
+
+    // Random height for each building
+    let h = random(50, height);
+
+    // Colour determined by height
+    let hue;
+    let saturation;
+    let brightness;
+
+    if (h > height / 2) {
+      // Tall buildings
+      hue = random(200, 320);
+      saturation = 90;
+      brightness = 80;
+    } else {
+      // Short buildings
+      hue = random(20, 80);
+      saturation = 40;
+      brightness = 95;
+    }
+
+    fill(hue, saturation, brightness);
+
+    // Draw building from bottom of canvas
+    rect(x, height - h, buildingWidth, h);
+  }
+}
